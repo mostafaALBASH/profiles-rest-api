2e580edbd13d19eef877b14fa040031ddbf786cb
MostafaALBASH <mostafa_elbesh@outlook.com>
2020-04-08 22:08:18 +0400

    6

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -4,9 +4,13 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from . import serializers
+from rest_framework import status
 
 class HelloApiView(APIView):
     """Test API View."""
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features."""
@@ -19,3 +23,30 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name."""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}!'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Put"""
+
+        return Response({'method':'put'})
+
+    def delete(self, request, pk=None):
+        """delete"""
+
+        return Response({'method':'delete'})
+
+    def patch(self, request, pk=None):
+        """Patch request, only updates fields provided in the request."""
+        return Response({'method':'Patch'})
